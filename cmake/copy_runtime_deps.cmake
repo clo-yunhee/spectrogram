4f8b6a09b669d24b60694888de6afbbde1519ed1
Clo Yun-Hee Dufour <cloyunhee@gmail.com>
2021-12-26 22:03:25 +0100

    Added portaudio dependency

---
+++ cmake/copy_runtime_deps.cmake
@@ -0,0 +1,68 @@
+#[[
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+
+]]
+
+get_filename_component(_target_dir ${TARGET_EXECUTABLE} DIRECTORY)
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    if(CMAKE_CROSSCOMPILING)
+        set(CMAKE_GET_RUNTIME_DEPENDENCIES_PLATFORM "windows+pe")
+        set(CMAKE_GET_RUNTIME_DEPENDENCIES_TOOL "objdump")
+        set(CMAKE_GET_RUNTIME_DEPENDENCIES_COMMAND "x86_64-w64-mingw32-objdump")
+    endif()
+
+    file(GET_RUNTIME_DEPENDENCIES
+        EXECUTABLES ${TARGET_EXECUTABLE}
+        RESOLVED_DEPENDENCIES_VAR _r_deps
+        UNRESOLVED_DEPENDENCIES_VAR _u_deps
+        PRE_EXCLUDE_REGEXES "api-ms-*" "ext-ms-*"
+        POST_EXCLUDE_REGEXES ".*system32/.*\\.dll" "${target_dir}/(?!fftw3).*\\.dll"
+        DIRECTORIES ${QT_RUNTIME_DIR} ${_target_dir}/../fftw-3.3.10 
+    )
+
+    foreach(_file ${_r_deps})
+        file(INSTALL ${_file} DESTINATION ${_target_dir})
+    endforeach()
+
+    foreach(_file ${_u_deps})
+        message(WARNING "Runtime dependency ${_file} could not be resolved.")
+    endforeach()
+
+    if(CMAKE_BUILD_TYPE STREQUAL Debug)
+        set(_d d)
+    else()
+        set(_d)
+    endif()
+
+    file(INSTALL ${QT_PLUGIN_DIR}/platforms/qwindows${_d}.dll DESTINATION ${_target_dir}/plugins/platforms)
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    file(INSTALL ${QT_PLUGIN_DIR}/platforms/libqxcb.so DESTINATION ${_target_dir}/plugins/platforms)
+endif()
---
+++ cmake/find_qt_root.cmake
@@ -0,0 +1,100 @@
+#[[
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+
+]]
+
+find_package(Qt REQUIRED NAMES Qt5)
+
+set(_qt_root "${Qt_DIR}/../../..")
+
+message(STATUS "Looking for Qt paths")
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    if(NOT CMAKE_CROSSCOMPILING)
+        set(QT_RUNTIME_DIR ${_qt_root}/bin)
+        set(QT_LIBRARY_DIR ${_qt_root}/lib)
+        set(QT_PLUGIN_DIR ${_qt_root}/plugins)
+    else()
+        set(QT_RUNTIME_DIR ${Qt_DIR}/../..)
+        set(QT_LIBRARY_DIR ${Qt_DIR}/../..)
+
+        find_path(_qt_prefix
+            NAMES qt5/plugins
+            PATHS
+                ${Qt_DIR}/../..
+            PATH_SUFFIXES
+                platforms
+                audio
+        )
+
+        set(QT_PLUGIN_DIR ${_qt_prefix}/qt5/plugins)
+    endif()
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    # Check if the default QT_ROOT is already correct. (If standalone Qt installation) 
+    if(EXISTS ${_qt_root}/plugins)
+        set(QT_RUNTIME_DIR ${_qt_root}/../..)
+        set(QT_LIBRARY_DIR ${_qt_root}/../..)
+        set(QT_PLUGIN_DIR ${_qt_root}/plugins)
+
+    else()
+        # This instead of lib/ subfolder because it can be in lib64.
+        set(QT_RUNTIME_DIR ${Qt_DIR}/../..)
+        set(QT_LIBRARY_DIR ${Qt_DIR}/../..)
+
+        find_path(_qt_prefix
+            NAMES qt5/plugins
+            PATHS
+                /usr/lib64
+                /usr/lib
+                /usr/libexec
+                /usr/local/lib64
+                /usr/local/lib
+                /usr/local/libexec
+                /opt/lib64
+                /opt/lib
+                /opt/libexec
+            PATH_SUFFIXES
+                platforms
+                audio
+        )
+
+        set(QT_PLUGIN_DIR ${_qt_prefix}/qt5/plugins)
+    endif()
+else()
+    message(AUTHOR_WARNING "  There is no known Qt 5 installation directory structure for this platform (${CMAKE_SYSTEM_NAME}). Will default to the Windows behaviour.")
+endif()
+
+get_filename_component(QT_RUNTIME_DIR ${QT_RUNTIME_DIR} ABSOLUTE)
+get_filename_component(QT_LIBRARY_DIR ${QT_LIBRARY_DIR} ABSOLUTE)
+get_filename_component(QT_PLUGIN_DIR ${QT_PLUGIN_DIR} ABSOLUTE)
+
+message(STATUS "  Qt runtime path: ${QT_RUNTIME_DIR}")
+message(STATUS "  Qt library path: ${QT_LIBRARY_DIR}")
+message(STATUS "  Qt plugin path: ${QT_PLUGIN_DIR}")
